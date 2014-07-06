d771eb131cb3f411a1116bccc44bc16e80fb6ca0
mzp <mzpppp@gmail.com>
2014-07-06 09:33:07 +0900

    first commit

---
+++ lib/ruboty/misawa.rb
@@ -0,0 +1,7 @@
+require "ruboty/misawa/version"
+
+module Ruboty
+  module Misawa
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/misawa/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module Misawa
+    VERSION = "0.0.1"
+  end
+end
