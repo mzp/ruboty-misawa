d9373611bc7965fcf93cea9f280c3d1b0957fe70
june29 <june29.jp@gmail.com>
2015-07-31 17:06:26 +0900

    point-to-new-url

--- lib/ruboty/handlers/misawa.rb
+++ lib/ruboty/handlers/misawa.rb
@@ -7,7 +7,7 @@ module Ruboty
   module Handlers
     class Misawa < Base
       on /misawa( me)? ?(?<keyword>.+)?/, name: 'misawa', description: 'Generate horesase boy image matching with the keyword'
-      env :MISAWA_ENDPOINT, "misawa db(default: http://horesase-boys.herokuapp.com/meigens.json)", optional: true
+      env :MISAWA_ENDPOINT, "misawa db(default: http://horesase.github.io/horesase-boys/meigens.json)", optional: true
 
       def misawa(message={})
         keyword = message[:keyword]
@@ -26,9 +26,8 @@ module Ruboty
 
       private
       def meigens
-        @meigens ||= JSON.parse(open(ENV['MISAWA_ENDPOINT'] || 'http://horesase-boys.herokuapp.com/meigens.json').read)
+        @meigens ||= JSON.parse(open(ENV['MISAWA_ENDPOINT'] || 'http://horesase.github.io/horesase-boys/meigens.json').read)
       end
     end
   end
 end
-
